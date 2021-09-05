5768105cb5211a01fcb6ab48a9a1bc64d2788ba0
magus <martingramajo08@gmail.com>
2021-09-05 18:29:17 -0300

    agrego spinner de carga para mostrar los pokemons

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import NavReact from "./components/NavReact";
 import Pokedex from "./components/Pokedex";
 import Searchbar from "./components/Searchbar";
 import { getPokemonData, getPokemons } from "./api";
+import { Spinner } from "react-bootstrap";
 
 
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState();
+  const [total, setTotal] = useState();
+  const [loading, setLoading] = useState(true); 
 
   const fetchPokemons = async () => {
     try {
       const data = await getPokemons();
-      console.log("file: App.js ~ line 17 ~ fetchPokemons ~ data", data)
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url) // array de promesa con datos especifico de cada pokemon. crea 10 promesas para cada pokemon.
       })
       const results = await Promise.all(promises) // con el await hacemos esperar al codigo hasta que regrese el array de promesas de la linea 19.
       setPokemons(results)
+      setLoading(false);
 
     } catch (error) {
     }
@@ -33,7 +37,11 @@ function App() {
     <div >
       <NavReact />
       <Searchbar />
+      {loading ? <div className="text-center">
+        <Spinner  animation="border" variant="danger" />
+        Cargando pokemons... </div> :
       <Pokedex pokemons={pokemons} />
+     }
     </div>
   );
 }
