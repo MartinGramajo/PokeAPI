65d56bf9e07729823f2e692d511d5959f36d3e4d
magus <martingramajo08@gmail.com>
2021-09-06 12:53:40 -0300

    agrego CSS all navbar

--- src/components/NavReact.jsx
+++ src/components/NavReact.jsx
@@ -5,12 +5,19 @@ import "./navreact.css";
 export default function NavReact() {
   const imgUrl =
     "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
+  const pikachu = "https://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png"
   return (
     <div>
-      <Navbar className="nav" bg="dark" expand="lg">
+      <Navbar className="nav"   fixed="top" bg="dark" expand="lg">
         <div>
           <Navbar.Brand href="#home">
             <div>
+            <img
+                className=" mt-3"
+                style={{width:'100px'}}
+                src={pikachu}
+                alt="PokeApi-logo"
+              />
               <img
                 className="navbar-image mt-3"
                 src={imgUrl}
