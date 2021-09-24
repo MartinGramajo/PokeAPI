409847372064a8039c90f180ee51b5f2c39c0b2d
magus <martingramajo08@gmail.com>
2021-09-24 15:45:35 -0300

    agrego css al componente pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -17,28 +17,34 @@ const Pokemon = (props) => {
   };
 
   return (
-      <div className="pokemon-card p-1 mb-3">
-        <div className="pokemon-img">
-            <h3 className="text-white text-center bg-dark">{pokemon.name} #{pokemon.id} </h3>
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <div className="container pokemon-card p-1 mb-3" style={{width:'350px'}}>
+        <div className="box pokemon-img">
+          <img className="pokemon-fondo " src={pokemon.sprites.front_default} alt={pokemon.name}  />
+        <h3 className="text-dark text-center">{pokemon.name} <span className="text-black-50"> {pokemon.weight}Hp </span>  </h3>
+        <span className=" text-black-50 text-center"> {pokemon.base_experience} exp </span>
         </div>
-        <div className="card-body p-3 bg-dark">
-          <div className="card-top d-flex flex-row  justify-content-between align-items-center">
-          </div>
-          <div className="card-bottom d-flex flex-row  justify-content-between align-items-center ">
+        <div className="card-footer p-3 ">
+          <div className="d-flex flex-row  justify-content-between align-items-center ">
             <div className="pokemon-type d-flex ">
               {pokemon.types.map((type, idx) => {
                 return (
                   <div
-                    className="bg-danger rounded-2 p-1 text-white"
+                    className="pokemon-type  rounded-2 p-1 text-dark"
                     style={{ marginRight: "15px" }}
                     key={idx}
                   >
-                    {type.type.name}
+                    <div className="fs-5 text-black-50">
+                      {type.type.name}
+                    </div>
+                    <div className="text-center fs-6">
+                      type
+                    </div>
                   </div>
                 );
               })}
-            </div>
+          </div>
+
+            <div>
             <Button
               className="bg-white"
               onClick={clickHeart}
@@ -46,19 +52,7 @@ const Pokemon = (props) => {
             >
               <div className="m-auto pokemon-favorite bg-white"> {heart} </div>
             </Button>
-          </div>
-          <div className="d-flex text-white ">
-            {pokemon.abilities.map((ability, idx) => {
-              return (
-                <div
-                  className="bg-warning rounded-2 p-1  text-dark mt-3"
-                  style={{ marginRight: "10px" }}
-                  key={idx}
-                >
-                  {ability.ability.name}
-                </div>
-              );
-            })}
+            </div>
           </div>
         </div>
       </div>
